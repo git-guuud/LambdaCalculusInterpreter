8e8a5e82424ce7c36d751fccd5d75add7d5a41ff
Shivam Maurya <mauryashivam6344@gmail.com>
2025-06-21 01:42:42 +0530

    deployment working

---
+++ www/LambdaCalculusInterpreter_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const generate_tree: (a: number, b: number) => [number, number];
+export const beta_reduce_once: () => [number, number];
+export const get_token_rep: () => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
